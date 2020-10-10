7e4a1565e7d82d341335ef9314d8af6ad13c4487
marshi <mashi3648@gmail.como>
2020-10-10 15:23:26 +0900

    sized box.

--- lib/signin/signin_page.dart
+++ lib/signin/signin_page.dart
@@ -7,32 +7,39 @@ class SigninPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final double height = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          children: [
-            Text(
-              "Welcome Back",
-              style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 28),
-            ),
-            Text(
-              "Sign in with your email and password \n or continue with social media",
-              textAlign: TextAlign.center,
-            ),
-            DefaultTextInputForm("email"),
-            DefaultTextInputForm(
-              "password",
-              SvgPicture.asset("assets/icons/Lock.svg"),
-            ),
-            Row(),
-            FlatButton(onPressed: null, child: Text("data")),
-            Row(),
-            Text("Don't have an account?")
-          ],
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              SizedBox(height: height * 0.04),
+              Text(
+                "Welcome Back",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 28),
+              ),
+              Text(
+                "Sign in with your email and password \n or continue with social media",
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: height * 0.08),
+              DefaultTextInputForm("email"),
+              SizedBox(height: 30),
+              DefaultTextInputForm(
+                "password",
+                SvgPicture.asset("assets/icons/Lock.svg"),
+              ),
+              Row(),
+              FlatButton(onPressed: null, child: Text("data")),
+              Row(),
+              Text("Don't have an account?")
+            ],
+          ),
         ),
       ),
     );
